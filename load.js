42d9177d1e61d6819e15e46e9457703aaadd06fb
AzureSphere <dustin.jason.lee@gmail.com>
2021-08-29 14:39:12 -0400

    Changed overflow before and after load

--- load.js
+++ load.js
@@ -1,5 +1,8 @@
 window.addEventListener('load', () => {
+    let body = document.getElementsByTagName("body")[0];
+    body.style.overflow = "hidden";
     setTimeout(() => {
         document.getElementById("loader").style.display = "none";
+        body.style.overflow = "auto"
     }, 500);
 });
