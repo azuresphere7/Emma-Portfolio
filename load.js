2a87543bdde6a1325e8f19e2cda614c2529098c2
AzureSphere <dustin.jason.lee@gmail.com>
2021-09-06 00:22:21 -0400

    Compressed images and fixed loader

--- load.js
+++ load.js
@@ -1,6 +1,7 @@
+let body = document.getElementsByTagName("body")[0];
+body.style.overflow = "hidden";
+
 window.addEventListener('load', () => {
-    let body = document.getElementsByTagName("body")[0];
-    body.style.overflow = "hidden";
     setTimeout(() => {
         document.getElementById("loader").style.display = "none";
         body.style.overflow = "auto"
